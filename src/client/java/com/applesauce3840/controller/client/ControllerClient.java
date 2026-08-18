2d04c2d3b6c1b812884d59f46f07117e86ef5bc4
bob369 <zinengxiong@gmail.com>
2026-08-17 22:17:05 -0700

    Initial commit

---
+++ src/client/java/com/applesauce3840/controller/client/ControllerClient.java
@@ -0,0 +1,10 @@
+package com.applesauce3840.controller.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ControllerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
